3a849928ec6c22b604764fa4a9a1edeed3361093
unknown <dohailong9901@gmail.com>
2022-04-16 01:11:12 +0700

    complete services page

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,124 @@
 import React from 'react'
+import './Services.scss';
+import { BsBookmarkCheckFill } from 'react-icons/bs'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section id='service'>
+      <h5>What I Offer ?</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UX/UI design</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>Embedded System</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>FrontEnd Developer</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsBookmarkCheckFill className='service__list--icon' />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
   )
 }
 
