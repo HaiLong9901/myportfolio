09de1a28691119645fcb633d22ed41cf6ffebb93
unknown <dohailong9901@gmail.com>
2022-04-17 00:39:49 +0700

    complete portfolio page

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,8 +1,116 @@
 import React from 'react'
+import './Portfolio.scss'
+import IMG from '../../assets/me.jpg'
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section className='portfolio'>
+      <h5>My recent work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item--image">
+            <img src={IMG} alt="portfolio" />
+          </div>
+          <h3>Title</h3>
+          <div className="portfolio__item--cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live code</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item--image">
+            <img src={IMG} alt="portfolio" />
+          </div>
+          <h3>Title</h3>
+          <div className="portfolio__item--cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live code</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item--image">
+            <img src={IMG} alt="portfolio" />
+          </div>
+          <h3>Title</h3>
+          <div className="portfolio__item--cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live code</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item--image">
+            <img src={IMG} alt="portfolio" />
+          </div>
+          <h3>Title</h3>
+          <div className="portfolio__item--cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live code</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item--image">
+            <img src={IMG} alt="portfolio" />
+          </div>
+          <h3>Title</h3>
+          <div className="portfolio__item--cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live code</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item--image">
+            <img src={IMG} alt="portfolio" />
+          </div>
+          <h3>Title</h3>
+          <div className="portfolio__item--cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live code</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item--image">
+            <img src={IMG} alt="portfolio" />
+          </div>
+          <h3>Title</h3>
+          <div className="portfolio__item--cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live code</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item--image">
+            <img src={IMG} alt="portfolio" />
+          </div>
+          <h3>Title</h3>
+          <div className="portfolio__item--cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live code</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item--image">
+            <img src={IMG} alt="portfolio" />
+          </div>
+          <h3>Title</h3>
+          <div className="portfolio__item--cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live code</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item--image">
+            <img src={IMG} alt="portfolio" />
+          </div>
+          <h3>Title</h3>
+          <div className="portfolio__item--cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live code</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
+import './Testimonials.scss'
+import AVT from '../../assets/me.jpg'
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className='testimonial'>
+          <div className="client__avatar">
+            <img src={AVT} alt="avatar" />
+          </div>
+          <h5 className='client__name'>Hello world</h5>
+          <small className='client__review'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam dicta voluptate quaerat suscipit, molestias rem porro dolor voluptatibus quas ducimus.</small>
+        </article>
+        <article className='testimonial'>
+          <div className="client__avatar">
+            <img src={AVT} alt="avatar" />
+          </div>
+          <h5 className='client__name'>Hello world</h5>
+          <small className='client__review'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam dicta voluptate quaerat suscipit, molestias rem porro dolor voluptatibus quas ducimus.</small>
+        </article>
+        <article className='testimonial'>
+          <div className="client__avatar">
+            <img src={AVT} alt="avatar" />
+          </div>
+          <h5 className='client__name'>Hello world</h5>
+          <small className='client__review'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam dicta voluptate quaerat suscipit, molestias rem porro dolor voluptatibus quas ducimus.</small>
+        </article>
+        <article className='testimonial'>
+          <div className="client__avatar">
+            <img src={AVT} alt="avatar" />
+          </div>
+          <h5 className='client__name'>Hello world</h5>
+          <small className='client__review'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam dicta voluptate quaerat suscipit, molestias rem porro dolor voluptatibus quas ducimus.</small>
+        </article>
+        <article className='testimonial'>
+          <div className="client__avatar">
+            <img src={AVT} alt="avatar" />
+          </div>
+          <h5 className='client__name'>Hello world</h5>
+          <small className='client__review'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam dicta voluptate quaerat suscipit, molestias rem porro dolor voluptatibus quas ducimus.</small>
+        </article>
+        <article className='testimonial'>
+          <div className="client__avatar">
+            <img src={AVT} alt="avatar" />
+          </div>
+          <h5 className='client__name'>Hello world</h5>
+          <small className='client__review'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam dicta voluptate quaerat suscipit, molestias rem porro dolor voluptatibus quas ducimus.</small>
+        </article>
+      </div>
+    </section>
   )
 }
 
