9c5f9fcebc93d00b32d748a028a9ce823f4fe950
unknown <dohailong9901@gmail.com>
2022-04-21 00:07:01 +0700

    complete testimonials page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import './Contact.scss'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container container__contact">
+        <div className="contact__options">
+          <article className="contact__option">
+            <h4>Email</h4>
+          </article>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import './Testimonials.scss'
 import AVT from '../../assets/me.jpg'
+import { Navigation, Pagination} from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const Testimonials = () => {
   return (
@@ -8,50 +17,50 @@ const Testimonials = () => {
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials__container">
-        <article className='testimonial'>
+      <Swiper className="container testimonials__container"
+              modules={[Navigation, Pagination]}
+              spaceBetween={40}
+              slidesPerView={1}
+              pagination={{ clickable: true }}
+              // scrollbar={{ draggable: true }}
+      >
+        <SwiperSlide className='testimonial'>
           <div className="client__avatar">
             <img src={AVT} alt="avatar" />
           </div>
           <h5 className='client__name'>Hello world</h5>
           <small className='client__review'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam dicta voluptate quaerat suscipit, molestias rem porro dolor voluptatibus quas ducimus.</small>
-        </article>
-        <article className='testimonial'>
+        </SwiperSlide>
+        <SwiperSlide className='testimonial'>
           <div className="client__avatar">
             <img src={AVT} alt="avatar" />
           </div>
           <h5 className='client__name'>Hello world</h5>
           <small className='client__review'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam dicta voluptate quaerat suscipit, molestias rem porro dolor voluptatibus quas ducimus.</small>
-        </article>
-        <article className='testimonial'>
+        </SwiperSlide>
+        <SwiperSlide className='testimonial'>
           <div className="client__avatar">
             <img src={AVT} alt="avatar" />
           </div>
           <h5 className='client__name'>Hello world</h5>
           <small className='client__review'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam dicta voluptate quaerat suscipit, molestias rem porro dolor voluptatibus quas ducimus.</small>
-        </article>
-        <article className='testimonial'>
+        </SwiperSlide>
+        <SwiperSlide className='testimonial'>
           <div className="client__avatar">
             <img src={AVT} alt="avatar" />
           </div>
           <h5 className='client__name'>Hello world</h5>
           <small className='client__review'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam dicta voluptate quaerat suscipit, molestias rem porro dolor voluptatibus quas ducimus.</small>
-        </article>
-        <article className='testimonial'>
+        </SwiperSlide>
+        <SwiperSlide className='testimonial'>
           <div className="client__avatar">
             <img src={AVT} alt="avatar" />
           </div>
           <h5 className='client__name'>Hello world</h5>
           <small className='client__review'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam dicta voluptate quaerat suscipit, molestias rem porro dolor voluptatibus quas ducimus.</small>
-        </article>
-        <article className='testimonial'>
-          <div className="client__avatar">
-            <img src={AVT} alt="avatar" />
-          </div>
-          <h5 className='client__name'>Hello world</h5>
-          <small className='client__review'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam dicta voluptate quaerat suscipit, molestias rem porro dolor voluptatibus quas ducimus.</small>
-        </article>
-      </div>
+        </SwiperSlide>
+        
+      </Swiper>
     </section>
   )
 }
