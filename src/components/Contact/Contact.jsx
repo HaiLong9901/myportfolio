b032a46f65006ec39bad981b3405a637b269be73
unknown <dohailong9901@gmail.com>
2022-04-22 00:32:28 +0700

    complete contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Contact.scss'
+import { HiOutlineMail } from 'react-icons/hi'
+import { BsMessenger } from 'react-icons/bs'
+import { SiZalo } from 'react-icons/si'
 
 const Contact = () => {
   return (
@@ -7,12 +10,34 @@ const Contact = () => {
       <h5>Get In Touch</h5>
       <h2>Contact Me</h2>
 
-      <div className="container container__contact">
+      <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
+            <HiOutlineMail className='contact__option-icon' />
             <h4>Email</h4>
+            <h5>dohailong9901@gmail.com</h5>
+            <a href="mailto:dohailong9901@gmail.com">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsMessenger className='contact__option-icon' />
+            <h4>Messenger</h4>
+            <h5>Hải Long</h5>
+            <a href="https://m.me/" target='_blank'>Send a message</a>
+          </article>
+          <article className="contact__option">
+            <SiZalo className='contact__option-icon' />
+            <h4>Zalo</h4>
+            <h5>0356789100</h5>
+            <a href="https://chat.zalo.me/">Send a message</a>
           </article>
         </div>
+
+        <form action="">
+          <input type="text" placeholder='Enter your full name' name='name' required />
+          <input type="email" placeholder='Enter your email' name='email' required />
+          <textarea name="message" rows="7" placeholder='Enter your message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send</button>
+        </form>
       </div>
     </section>
   )
